c7e93f5734a0d4090fb3504bd411454687f447a6
Matheus Muniz <matheussk2015@gmail.com>
2023-04-27 22:49:24 -0300

    initial project

---
+++ Back/src/ProEventos.API/Controllers/EventoController.cs
@@ -0,0 +1,56 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.Extensions.Logging;
+using ProEventos.API.Models;
+
+namespace ProEventos.API.Controllers
+{
+    [ApiController]
+    [Route("api/[controller]")]
+    public class EventoController : ControllerBase
+    {
+        public IEnumerable<Evento> _evento = new Evento[]
+        {
+            new Evento()
+            {
+                EventoId = 1,
+                Local = "Jardins",
+                DataEvento = DateTime.Now.AddDays(2).ToString("dd/MM/yyyy"),
+                Tema = "Angular",
+                QtdPessoas = 100,
+                Lote = "1º Lote",
+                ImagemURL = "foto.png"
+            },
+            new Evento()
+            {
+                EventoId = 2,
+                Local = "Capao Redondo",
+                DataEvento = DateTime.Now.AddDays(3).ToString("dd/MM/yyyy"),
+                Tema = ".NET Core",
+                QtdPessoas = 120,
+                Lote = "2º Lote",
+                ImagemURL = "foto2.png"
+            }
+        };
+
+        public EventoController()
+        {
+           
+        }
+
+        [HttpGet]
+        public IEnumerable<Evento> Get()
+        {
+            return _evento;
+        }
+
+        [HttpGet("{id}")]
+        public IEnumerable<Evento> GetById(int id)
+        {
+            return _evento.Where(e => e.EventoId == id);
+        }
+    }
+}
