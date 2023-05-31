2fb8b1b72fecac54e4c7388a79b41a56fab9df4b
Matheus Muniz <matheussk2015@gmail.com>
2023-05-30 21:45:29 -0300

    Finalizacao_Tela_Eventos

--- Front/ProEventos-App/src/app/eventos/eventos.component.ts
+++ Front/ProEventos-App/src/app/eventos/eventos.component.ts
@@ -9,6 +9,28 @@ import { Component, OnInit } from '@angular/core';
 export class EventosComponent implements OnInit{
 
   public eventos: any = [];
+  public eventosFilters: any = [];
+  widthImg: number = 150;
+  marginImg: number = 2;
+  showImg: boolean = true;
+  private _filterList: string = '';
+
+  public get filterList(): string {
+    return this._filterList;
+  }
+
+  public set filterList(value: string) {
+    this._filterList = value;
+    this.eventosFilters = this._filterList ? this.filterEvent(this._filterList) : this.eventos;
+  }
+
+  filterEvent(filterBy: string): any {
+    filterBy = filterBy.toLocaleLowerCase();
+    return this.eventos.filter(
+      (evento: {tema: string; local: string;}) => evento.tema.toLocaleLowerCase().indexOf(filterBy) !== -1 ||
+      evento.local.toLocaleLowerCase().indexOf(filterBy) !== -1
+    );
+  }
 
   constructor(private http: HttpClient) {}
 
@@ -16,9 +38,16 @@ export class EventosComponent implements OnInit{
     this.getEventos();
   }
 
+  alterarImagem() {
+    this.showImg = !this.showImg;
+  }
+
   public getEventos(): void{
     this.http.get('https://localhost:44395/api/eventos').subscribe(
-      response => this.eventos = response,
+      response => {
+        this.eventos = response;
+        this.eventosFilters = this.eventos;
+      },
       error => console.log(error)
     );
   }
